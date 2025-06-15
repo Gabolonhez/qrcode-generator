609b1ada5046ae847a2f2da135d31196103d51a5
Gabriel BF <gbbolonhez@gmail.com>
2025-06-15 13:34:44 -0300

    feat: adding password generator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import prompt from 'prompt';
-import mainPrompt from "./prompts/prompt-main.js";
+import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
 import createQRCode from './services/qr-code/create.js';
 import createPassword from './services/password/create.js';
+import chalk from 'chalk';
 
 async function main () {
-    prompt.get(mainPrompt, async (err, choose) => {
+    prompt.get(promptSchemaMain, async (err, choose) => {
+
+        if(err) {
+            console.log(chalk.red.bold("An error occurred", err));
+        }
+
         if (choose.select == 1) await createQRCode();
         if (choose.select == 2) await createPassword();
 
--- src/prompts-schema/prompt-schema-main.js
+++ src/prompts-schema/prompt-schema-main.js
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 
-const mainPrompt = [
+const promptSchemaMain = [
     {
         name: "select",
         description: chalk.blue.bold("Escolha a ferramenta (1 - QR Code ou 2 - Password)"),
@@ -10,4 +10,4 @@ const mainPrompt = [
     }
 ]
 
-export default mainPrompt;
+export default promptSchemaMain;
